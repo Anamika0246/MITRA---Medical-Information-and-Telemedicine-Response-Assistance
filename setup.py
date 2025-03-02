2e1d5e6794cc7b00a15dfa97bf186a036f25e3c1
Anamika0246 <gorgeousanamika25@gmail.com>
2025-03-02 16:57:16 +0530

    Created the Folder Structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'MITRA',
+    version= '0.0.0',
+    author= 'Anamika Tiwari',
+    author_email= 'anamikatiwari0246@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
